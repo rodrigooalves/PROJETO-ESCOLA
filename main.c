0933f7042cb498df3759d3a9714647833755c739
Rodrigo Alves <119018810+rodrigooalves@users.noreply.github.com>
2025-04-06 12:30:28 -0300

    Estrutura criada para o menu e modulo aluno. falta somente as funções

--- main.c
+++ main.c
@@ -1,12 +1,81 @@
 #include <stdio.h>
+#define TAM_ALUNO 3
+
 int main () {
+    int option;
+    //int listAluno[TAM_ALUNO]; //VETOR ALUNOS
+    int out = 0;
 
-    /*int sair = 0;
-    printf("PROJETO ESCOLA!");
-    while (!sair) {
-        
+    
+    while(!out) {
+        printf("--------> MENU ESCOLA <-------\n");    
+        printf("##############################\n");    
+        printf(" 1 -> Portal do Aluno. <-\n");    
+        printf(" 2 -> Portal do Professor. <- \n");    
+        printf(" 3 -> Portal da Disciplina. <-\n");    
+        printf(" 0 -> Sair do Programa. <-\n\n"); 
+        scanf("%d", &option);
 
-        //teste de branch
-    }*/
+        switch(option) {
+            case 1: { //MODULO ALUNO
+                int optionAluno;
+                int outAluno = 0;
+                printf(" ---> PORTAL ALUNO <---\n");
+
+                while(!outAluno){
+                    printf("#########################\n");
+                    printf("1 --> Cadastrar aluno. <--\n");
+                    printf("2 --> Listar aluno. <--\n");
+                    printf("3 --> Atualizar aluno. <--\n");
+                    printf("4 --> Excluir aluno. <--\n");
+                    printf("0 --> Voltar para o PORTAL. <--\n\n");
+                    scanf("%d",&optionAluno);
+                    switch(optionAluno){
+                        case 1: {
+                                // FUNÇÃO DE CADASTRAR
+                            break;
+                        }
+                        case 2: {
+                                // FUNÇÃO DE LISTAR
+                            break;
+                        }
+                        case 3: {
+                            // LISTAR ATUALIZAR
+                            break;
+                        }
+                        case 4: {
+                            // EXCLUIR ALUNO
+                            break;
+                        }
+                        case 0: {
+                            outAluno = 1;
+                            break;
+                        }
+                        default: {
+                            printf("Opcao Invalida\n");
+                        }
+                    }
+                }   
+                break;
+            }
+            case 2: { // MODULO PROFRESSOR
+
+                break;
+            }
+            case 3: { // MODULO DISCIPLINA
+
+                break;
+            }
+            case 0: { // MODULO SAIR
+                out = 1;
+                break;
+            }
+            default: {
+                printf("-> Opcao invalida <-\n");
+            }
+        }
+
+    }
+    printf("Encerrando o programa...\n");
 return 0;
 }
